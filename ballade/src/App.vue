96e133bba4f729b890e70f6faeac1656a650f9e7
sunmy <sunmengyuan@wanmeizhensuo.com>
2017-11-20 14:32:43 +0800

    代码整理

---
+++ ballade/src/App.vue
@@ -0,0 +1,20 @@
+<template>
+    <div id="app">
+        <HelloWorld/>
+    </div>
+</template>
+
+<script>
+    import HelloWorld from './components/HelloWorld'
+
+    export default {
+        name: 'app',
+        components: {
+            HelloWorld
+        }
+    }
+</script>
+
+<style>
+
+</style>
---
+++ ballade/src/components/HelloWorld.vue
@@ -0,0 +1,16 @@
+<template>
+    <h1>Welcome</h1>
+</template>
+
+<script>
+    export default {
+        name: 'HelloWorld',
+        data () {
+            return {}
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
