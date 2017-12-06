9cec62d1bf83671cdaf1f35eff20ff097141b3a6
sunmy <sunmengyuan@wanmeizhensuo.com>
2017-12-06 19:08:55 +0800

    公共部分打包分离

--- ballade/build/webpack.prod.conf.js
+++ ballade/build/webpack.prod.conf.js
@@ -21,8 +21,7 @@ function htmls () {
                 filename: path.resolve(__dirname, `../../dist/${routes[key].path}${routes[key].view}.html`),
                 template: './src/index.html',
                 inject: true,
-                // inlineSource: '^((?!vendor|manifest).)*(js|css)$',
-                inlineSource: '.(js|css)$',
+                inlineSource: '^((?!vendor|manifest).)*(js|css)$',
                 minify: {
                     removeComments: true,
                     collapseWhitespace: true,
