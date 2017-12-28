af4147606cac4a6743b797cee191fc872b5b91c4
sunmy <sunmengyuan@wanmeizhensuo.com>
2017-12-28 18:34:50 +0800

    组件对哈希值影响调研

---
+++ ballade/src/components/Comp.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="comp">{{ message }}</div>
+</template>
+
+<script>
+export default {
+    props: {
+        message: {
+            type: String,
+            default: '我是组件'
+        }
+    }
+}
+</script>
+
+<style scoped>
+.comp {
+    color: mediumpurple;
+}
+</style>
--- ballade/src/views/activity/Activity.vue
+++ ballade/src/views/activity/Activity.vue
@@ -2,12 +2,19 @@
     <section>
         <h4>活动</h4>
         <p>{{ message }}</p>
+        <comp></comp>
     </section>
 </template>
 
 <script>
+import Comp from '../../components/Comp'
+
 export default {
     name: 'Activity',
+
+    components: {
+        Comp
+    },
 
     data () {
         return {
