543e361cf9b64f9ef9961effce088fee31813933
sunmy <sunmengyuan@wanmeizhensuo.com>
2017-12-28 16:58:39 +0800

    html 打时间戳

--- ballade/gulpfile.js
+++ ballade/gulpfile.js
@@ -2,6 +2,7 @@ const gulp = require('gulp')
 const replace = require('gulp-replace-task')
 const rename = require('gulp-rename')
 const del = require('del')
+const through2 = require('through2');
 const routes = require('./src/routes')
 
 gulp.task('entries', function () {
@@ -17,6 +18,24 @@ gulp.task('entries', function () {
             }))
             .pipe(rename(`entries/${routes[key].view}.js`))
             .pipe(gulp.dest('./entry'))
+    }
+})
+
+gulp.task('stamps', function () {
+    for (let key in routes) {
+        let stamp = '';
+        let view = routes[key].view;
+        let path = routes[key].path.slice(0, -1);
+        gulp.src(`../dist/static/${view}.*.js`)
+            .pipe(through2.obj(function (chunk, enc, callback) {
+                stamp = chunk.path.split(`${view}.`)[1].split('.js')[0];
+                callback();
+            }))
+            .on('finish', function () {
+                gulp.src(`../dist/${path}/${view}.html`)
+                    .pipe(rename(`${view}.${stamp}.html`))
+                    .pipe(gulp.dest(`../dist/${path}`))
+            })
     }
 })
 
