3457d09d6ffa1c85af36e1f5975ce0b37e12b6fa
sunmy <sunmengyuan@wanmeizhensuo.com>
2017-12-29 10:56:50 +0800

    组件哈希值

--- ballade/src/views/post/Topic.vue
+++ ballade/src/views/post/Topic.vue
@@ -2,14 +2,20 @@
     <section>
         <h4>Topic</h4>
         <p>{{ message }}</p>
+        <comp message="我是日记帖"></comp>
     </section>
 </template>
 
 <script>
 import Common from '../../utils/common'
+import Comp from '../../components/Comp'
 
 export default {
     name: 'Topic',
+
+    components: {
+        Comp
+    },
 
     data () {
         return {
