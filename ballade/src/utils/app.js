997d8a766876fc54ea767b6aae338fac805c37b1
sunmy <sunmengyuan@wanmeizhensuo.com>
2018-01-23 14:02:16 +0800

    uri 解析

--- ballade/src/utils/app.js
+++ ballade/src/utils/app.js
@@ -7,7 +7,11 @@ const App = {
             var search = window.location.search
             var value = ''
             var tmp = []
-            if (search) tmp = search.slice(1).split('&')
+            if (search) {
+                tmp = (process.env.NODE_ENV === 'production')
+                    ? decodeURIComponent(search.split('uri=')[1]).split('?')[1].split('&')
+                    : search.slice(1).split('&')
+            }
             for (let i in tmp) {
                 if (key === tmp[i].split('=')[0]) {
                     value = tmp[i].split('=')[1]
