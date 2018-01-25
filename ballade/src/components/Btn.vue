5d962478823d953790e8c96ba9bb43fd0449ba8f
sunmy <sunmengyuan@wanmeizhensuo.com>
2018-01-25 16:58:16 +0800

    btn 封装

--- ballade/src/components/Btn.vue
+++ ballade/src/components/Btn.vue
@@ -15,200 +15,219 @@ $btns: (
     /* 纯色绿 */
     pureGreen: (
         normal: (
-            font: #FFF,
-            border: none,
+            fClr: #FFF,
+            bdClr: none,
             background: #3ADBD2
         ),
         active: (
-            font: #FFF,
-            border: none,
+            fClr: #FFF,
+            bdClr: none,
             background: #1FB2A7
         ),
         disable: (
-            font: #FFF,
-            border: none,
+            fClr: #FFF,
+            bdClr: none,
             background: #D5D5D5
         )
     ),
     /* 纯色粉 */
     purePink: (
         normal: (
-            font: #FFF,
-            border: none,
+            fClr: #FFF,
+            bdClr: none,
             background: #FF7690
         ),
         active: (
-            font: #FFF,
-            border: none,
+            fClr: #FFF,
+            bdClr: none,
             background: #E75873
         ),
         disable: (
-            font: #FFF,
-            border: none,
+            fClr: #FFF,
+            bdClr: none,
             background: #D5D5D5
         )
     ),
     /* 纯色灰 */
     pureGrey: (
         normal: (
-            font: #666,
-            border: #E5E5E5,
+            fClr: #666,
+            bdClr: #E5E5E5,
             background: #F5F5F5
         ),
         active: (
-            font: #666,
-            border: #E5E5E5,
+            fClr: #666,
+            bdClr: #E5E5E5,
             background: #E5E5E5
         ),
         disable: (
-            font: #FFF,
-            border: #D5D5D5,
+            fClr: #FFF,
+            bdClr: #D5D5D5,
             background: #D5D5D5
         )
     ),
     /* 线框绿 */
     bdGreen: (
         normal: (
-            font: #3ADBD2,
-            border: #3ADBD2,
+            fClr: #3ADBD2,
+            bdClr: #3ADBD2,
             background: transparent
         ),
         active: (
-            font: #3ADBD2,
-            border: #3ADBD2,
+            fClr: #3ADBD2,
+            bdClr: #3ADBD2,
             background: transparent
         ),
         disable: (
-            font: #D5D5D5,
-            border: #D5D5D5,
+            fClr: #D5D5D5,
+            bdClr: #D5D5D5,
             background: transparent
         )
     ),
     /* 线框粉 */
     bdPink: (
         normal: (
-            font: #FF7690,
-            border: #FF7690,
+            fClr: #FF7690,
+            bdClr: #FF7690,
             background: transparent
         ),
         active: (
-            font: #FF7690,
-            border: #FF7690,
+            fClr: #FF7690,
+            bdClr: #FF7690,
             background: transparent
         ),
         disable: (
-            font: #D5D5D5,
-            border: #D5D5D5,
+            fClr: #D5D5D5,
+            bdClr: #D5D5D5,
             background: transparent
         )
     )
     /* 线框灰 */
     bdGrey: (
         normal: (
-            font: #666,
-            border: #666,
+            fClr: #666,
+            bdClr: #666,
             background: transparent
         ),
         active: (
-            font: #666,
-            border: #666,
+            fClr: #666,
+            bdClr: #666,
             background: transparent
         ),
         disable: (
-            font: #D5D5D5,
-            border: #D5D5D5,
+            fClr: #D5D5D5,
+            bdClr: #D5D5D5,
             background: transparent
         )
     ),
     /* 线框灰-弱 */
     bdGreyWeak: (
         normal: (
-            font: #CCC,
-            border: #CCC,
+            fClr: #CCC,
+            bdClr: #CCC,
             background: transparent
         ),
         active: (
-            font: #CCC,
-            border: #CCC,
+            fClr: #CCC,
+            bdClr: #CCC,
             background: transparent
         ),
         disable: (
-            font: #CCC,
-            border: #CCC,
+            fClr: #CCC,
+            bdClr: #CCC,
             background: transparent
         )
     ),
     /* 渐变绿-跑道圆 */
     gradGreenRound: (
         normal: (
-            font: #FFF,
-            border: none,
+            fClr: #FFF,
+            bdClr: none,
             background: linear-gradient(-120deg, #45E7DE 0%, #58CBEF 100%)
         ),
         active: (
-            font: #FFF,
-            border: none,
+            fClr: #FFF,
+            bdClr: none,
             background: linear-gradient(-120deg, #2ECCC4 0%, #42B8DD 100%)
         ),
         disable: (
-            font: #FFF,
-            border: none,
+            fClr: #FFF,
+            bdClr: none,
             background: #D5D5D5
         )
     ),
     /* 渐变粉-跑道圆 */
     gradPinkRound: (
         normal: (
-            font: #FFF,
-            border: none,
+            fClr: #FFF,
+            bdClr: none,
             background: linear-gradient(59deg, #FF5963 0%, #FF7096 100%)
         ),
         active: (
-            font: #FFF,
-            border: none,
+            fClr: #FFF,
+            bdClr: none,
             background: linear-gradient(59deg, #E74751 0%, #F4547E 100%)
         ),
         disable: (
-            font: #FFF,
-            border: none,
+            fClr: #FFF,
+            bdClr: none,
             background: #D5D5D5
         )
     ),
     /* 渐变绿 */
     gradGreen: (
         normal: (
-            font: #FFF,
-            border: none,
+            fClr: #FFF,
+            bdClr: none,
             background: linear-gradient(-131deg, #3CE6D9 0%, #51D3E7 100%)
         ),
         active: (
-            font: #FFF,
-            border: none,
+            fClr: #FFF,
+            bdClr: none,
             background: linear-gradient(-131deg, #34D1C5 0%, #45BCCF 100%)
         ),
         disable: (
-            font: #FFF,
-            border: none,
+            fClr: #FFF,
+            bdClr: none,
             background: #D5D5D5
         )
     ),
     /* 渐变粉 */
     gradPink: (
         normal: (
-            font: #FFF,
-            border: none,
+            fClr: #FFF,
+            bdClr: none,
             background: linear-gradient(48deg, #FF5963 0%, #FF7096 100%)
         ),
         active: (
-            font: #FFF,
-            border: none,
+            fClr: #FFF,
+            bdClr: none,
             background: linear-gradient(46deg, #E74751 0%, #F4547E 100%)
         ),
         disable: (
-            font: #FFF,
-            border: none,
+            fClr: #FFF,
+            bdClr: none,
             background: #D5D5D5
         )
     )
 )
+@mixin btnStyle ($fClr, $bdClr, $background) {
+    color: $fClr;
+    border-color: $bdClr;
+    background-color: $background;
+}
+@each $type, $status in $btns {
+    %btn-#{$type} {
+        $normal: map-get($status, normal);
+        $active: map-get($status, active);
+        $disable: map-get($status, disable);
+        @include btnStyle(map-values($normal)...);
+        &.active {
+            @include btnStyle(map-values($active)...);
+        }
+        &.disable {
+            @include btnStyle(map-values($disable)...);
+        }
+    }
+}
 </style>
