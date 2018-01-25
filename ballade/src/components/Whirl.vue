d3fec17ef641ea80ef7218872d3a32c6c59e1a16
sunmy <sunmengyuan@wanmeizhensuo.com>
2018-01-25 14:06:56 +0800

    whirl

--- ballade/src/components/Whirl.vue
+++ ballade/src/components/Whirl.vue
@@ -1,6 +1,8 @@
 <template>
     <div class="gm-whirl">
-        <span v-for="item in [1, 2, 3, 4, 5, 6, 7, 8]"></span>
+        <div class="whirl">
+            <span v-for="item in [1, 2, 3, 4, 5, 6, 7, 8]"></span>
+        </div>
     </div>
 </template>
 
@@ -12,14 +14,23 @@ export default {
 
 <style lang="scss" scoped>
 .gm-whirl {
-    position: relative;
-    width: 2rem;
-    height: 2rem;
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background-color: rgba(255, 255, 255, 0.5);
 }
-.gm-whirl span {
+.whirl {
+    position: relative;
+    width: 1rem;
+    height: 1rem;
+    @extend %gm-center;
+}
+.whirl span {
     position: absolute;
-    width: 0.4rem;
-    height: 0.4rem;
+    width: 0.2rem;
+    height: 0.2rem;
     border-radius: 50%;
     background-color: $clrLight;
     transform: scale(0);
@@ -37,45 +48,45 @@ export default {
 }
 
 @for $index from 0 through 7 {
-    .gm-whirl span:nth-child(#{$index}) {
+    .whirl span:nth-child(#{$index}) {
         animation-delay: $index*0.08s;
     }
 }
 
-.gm-whirl span:nth-child(1) {
+.whirl span:nth-child(1) {
     left: 0;
     top: 50%;
-    margin-top: -0.2rem;
+    margin-top: -0.1rem;
 }
-.gm-whirl span:nth-child(2) {
-    left: 0.28rem;
-    top: 0.28rem;
+.whirl span:nth-child(2) {
+    left: 0.14rem;
+    top: 0.14rem;
 }
-.gm-whirl span:nth-child(3) {
+.whirl span:nth-child(3) {
     left: 50%;
     top: 0;
-    margin-left: -0.2rem;
+    margin-left: -0.1rem;
 }
-.gm-whirl span:nth-child(4) {
-    top: 0.28rem;
-    right: 0.28rem;
+.whirl span:nth-child(4) {
+    top: 0.14rem;
+    right: 0.14rem;
 }
-.gm-whirl span:nth-child(5) {
+.whirl span:nth-child(5) {
     right: 0;
     top: 50%;
-    margin-top: -0.2rem;
+    margin-top: -0.1rem;
 }
-.gm-whirl span:nth-child(6) {
-    right: 0.28rem;
-    bottom: 0.28rem;
+.whirl span:nth-child(6) {
+    right: 0.14rem;
+    bottom: 0.14rem;
 }
-.gm-whirl span:nth-child(7) {
+.whirl span:nth-child(7) {
     bottom: 0;
     left: 50%;
-    margin-left: -0.2rem;
+    margin-left: -0.1rem;
 }
-.gm-whirl span:nth-child(8) {
-    bottom: 0.28rem;
-    left: 0.28rem;
+.whirl span:nth-child(8) {
+    bottom: 0.14rem;
+    left: 0.14rem;
 }
 </style>
