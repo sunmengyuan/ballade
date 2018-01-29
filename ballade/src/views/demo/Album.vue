fd1da3df5d177de2edb0a10398ab491e61cd2f1e
sunmy <sunmengyuan@wanmeizhensuo.com>
2018-01-29 17:38:25 +0800

    lazy img

--- ballade/src/views/demo/Album.vue
+++ ballade/src/views/demo/Album.vue
@@ -2,36 +2,34 @@
     <section class="gm-content">
         <div class="banner">
             <lazy-img :src="'http://hera.s.igengmei.com/2016/05/12/f7714d5dbd'"></lazy-img>
-            <lazy-img
-                :src="'http://hera.s.igengmei.com/2016/05/12/d3965a1d96'"
-                :ratio="0.6"></lazy-img>
+            <lazy-img :src="'http://hera.s.igengmei.com/2016/05/12/d3965a1d96'"></lazy-img>
         </div>
-        <div class="tofu1">
+        <div class="tofu1 gm-clear">
             <lazy-img
                 :src="'http://hera.s.igengmei.com/2016/05/12/43d8e50210-half'"
-                :ratio="0.5"
+                :ratio="1"
                 class="gm-item"></lazy-img>
             <lazy-img
                 :src="'http://hera.s.igengmei.com/2016/05/12/209463c376-half'"
-                :ratio="0.5"
+                :ratio="1"
                 class="gm-item"></lazy-img>
         </div>
-        <div class="tofu2">
+        <div class="tofu2 gm-clear">
             <lazy-img
                 :src="'http://hera.s.igengmei.com/2016/05/12/aed4e175be-half'"
-                :ratio="0.5"
+                :ratio="1"
                 class="gm-item"></lazy-img>
             <lazy-img
                 :src="'http://hera.s.igengmei.com/2016/05/12/aaf8e25546-half'"
-                :ratio="0.5"
+                :ratio="1"
                 class="gm-item"></lazy-img>
             <lazy-img
                 :src="'http://hera.s.igengmei.com/2016/05/12/073fb80f70-half'"
-                :ratio="0.5"
+                :ratio="1"
                 class="gm-item"></lazy-img>
             <lazy-img
                 :src="'http://hera.s.igengmei.com/2016/05/12/36bf70877f-half'"
-                :ratio="0.5"
+                :ratio="1"
                 class="gm-item"></lazy-img>
         </div>
     </section>
