1bcd082b8defecfacf7178059bea71b6fbec5bb1
sunmy <sunmengyuan@wanmeizhensuo.com>
2018-03-05 16:29:02 +0800

    ballade

--- ballade/src/views/post/ArticleDetail.vue
+++ ballade/src/views/post/ArticleDetail.vue
@@ -1,5 +1,16 @@
 <template>
-
+    <section class="gm-content">
+        <section class="common">
+            <img src="http://hera.s.igengmei.com/2016/05/12/f7714d5dbd" class="banner" />
+            <div class="bar-placeholder" :style="{ height: barHeight + 'px' }"></div>
+            <div class="article-detail">
+                <div class="author">
+                    <img src="http://pic.igengmei.com/2017/07/03/1034/41699b881fc0-thumb" />
+                    <span>阿茶茶</span>
+                </div>
+            </div>
+        </section>
+    </section>
 </template>
 
 <script>
@@ -8,8 +19,12 @@ export default {
 
     data () {
         return {
-
+            barHeight: 0
         }
+    },
+
+    created () {
+        this.barHeight = this.$router.query('title_bar_height')
     },
 
     methods: {}
@@ -17,5 +32,12 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-
+.bar-placeholder {
+    background-color: #FFF;
+}
+.common {
+    .banner {
+        width: 100%;
+    }
+}
 </style>
