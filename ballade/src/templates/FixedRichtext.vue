3d70da51086feeba65a23a515435b9ecb6b6d522
sunmy <sunmengyuan@wanmeizhensuo.com>
2018-03-06 12:02:01 +0800

    卡片 ui

--- ballade/src/templates/FixedRichtext.vue
+++ ballade/src/templates/FixedRichtext.vue
@@ -41,6 +41,9 @@ export default {
 <style lang="scss">
 @import "../assets/richtext";
 
+.gm-fixed-richtext .gm-tiny-scale {
+    @extend %gm-tiny-scale;
+}
 .gm-fixed-richtext {
     div[data-type][data-info] {
         margin: .3rem 0;
@@ -53,29 +56,74 @@ export default {
     p:last-child {
         margin-bottom: 0;
     }
-    .tag {
+}
+.gm-fixed-richtext .tag {
+    position: absolute;
+    right: 0;
+    bottom: 8px;
+    font-size: 14px;
+    line-height: 26px;
+    color: #FFF;
+    padding: 0 12px 0 15px;
+    border-top-left-radius: 13px;
+    border-bottom-left-radius: 13px;
+    background-color: $fClr7690;
+    &:before {
+        content: "";
         position: absolute;
-        right: 0;
-        bottom: 8px;
-        font-size: 14px;
-        line-height: 26px;
-        color: #FFF;
-        padding: 0 12px 0 15px;
-        border-top-left-radius: 13px;
-        border-bottom-left-radius: 13px;
-        background-color: $fClr7690;
-        &:before {
-            content: "";
-            position: absolute;
-            right: -3px;
-            bottom: 26px;
-            width: 0;
-            height: 0;
-            border: 3px solid transparent;
-            border-left-color: #D22143;
-            border-top-width: 2px;
-            border-bottom-width: 0;
-        }
+        right: -3px;
+        bottom: 26px;
+        width: 0;
+        height: 0;
+        border: 3px solid transparent;
+        border-left-color: #D22143;
+        border-top-width: 2px;
+        border-bottom-width: 0;
+    }
+}
+.gm-fixed-richtext a.card {
+    text-decoration: none;
+    color: $fClrTitle;
+    ul, li {
+        list-style-type: none;
+        margin: 0;
+    }
+    img {
+        margin: 0;
+    }
+}
+.gm-fixed-richtext {
+    a.service-card,
+    a.expert-card {
+        position: relative;
+        height: 1.72rem;
+        padding-right: 3px;
+        margin-right: -3px;
+        overflow: hidden;
+    }
+    .service-card > div,
+    .expert-card > div {
+        position: relative;
+        height: 3.44rem;
+        padding: .48rem .48rem .48rem 3.28rem;
+        margin: -.86rem -50%;
+        border: 2px solid #F0F0F0;
+        border-radius: 6px;
+    }
+    .service-card img,
+    .expert-card img {
+        position: absolute;
+        left: .48rem;
+        top: .48rem;
+        width: 2.4rem;
+        height: 2.4rem;
+        border-radius: 6px;
+    }
+    .service-card h4,
+    .expert-card h4 {
+        font-size: .6rem;
+        line-height: .78rem;
+        margin-top: -.1rem;
     }
 }
 </style>
